35e42e3c82ad7d75e8c6cded4d5aa112546225dd
Simon Wright <simmo.wright@outlook.com>
2020-09-17 21:50:49 +1000

    Reorganise

---
+++ socialape-functions/functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCvC3D2dSgfPRWtqwtvWX6NFFxAxSxNAfI",
+    authDomain: "socialape-5121f.firebaseapp.com",
+    databaseURL: "https://socialape-5121f.firebaseio.com",
+    projectId: "socialape-5121f",
+    storageBucket: "socialape-5121f.appspot.com",
+    messagingSenderId: "102333852698",
+    appId: "1:102333852698:web:74f9366ded9bcbd13875a3",
+    measurementId: "G-8N79C94RR7",
+};
